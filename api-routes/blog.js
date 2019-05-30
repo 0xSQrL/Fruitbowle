4c8fdef40929cfcaa065cb2fdc5d5492a5dcb0fa
William Silloway <wsilloway@gmail.com>
2019-05-30 14:21:36 -0400

    Added blog file; added high level requirements;

---
+++ api-routes/blog.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const db = require.main.require('./../database');
+
+//Functions
+////////////
+//Write article
+//Read article
+//Insert redaction
+//Add comments
+//Edit comments
+//Delete comments
+//Rate Comments
+//Reply to comments
